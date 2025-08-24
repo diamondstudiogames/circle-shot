b849a3c8d6890404a6b23d7adf3d99607712b220
Summersay415 <summersay415@gmail.com>
2025-08-24 23:14:38 +0700

    Исправления багов

---
+++ game/skills/time_stop/time_stop.gd
@@ -0,0 +1,38 @@
+extends Skill
+
+var _use_scene: PackedScene = preload("uid://bquhtboou21kv")
+@onready var _timer: Timer = $Timer
+
+func _use() -> void:
+	block_cooldown()
+	player.make_immobile()
+	player.block_weapon_usage()
+	player.block_turning()
+	
+	($AnimationPlayer as AnimationPlayer).play(&"use")
+	_timer.start(0.5)
+	await _timer.timeout
+	
+	player.process_mode = Node.PROCESS_MODE_PAUSABLE
+	if multiplayer.is_server():
+		var time_stop_use: Node2D = _use_scene.instantiate()
+		time_stop_use.position = player.global_position
+		_other_parent.add_child(time_stop_use, true)
+		time_stop_use.tree_exiting.connect(_on_time_stop_use_tree_exiting)
+	
+	_timer.start(1.0)
+	await _timer.timeout
+	player.unmake_immobile()
+	player.unblock_weapon_usage()
+	player.unblock_turning()
+
+
+@rpc("reliable", "authority", "call_local", 3)
+func _end_time_stop() -> void:
+	player.process_mode = Node.PROCESS_MODE_INHERIT
+	unblock_cooldown()
+
+
+func _on_time_stop_use_tree_exiting() -> void:
+	if not is_queued_for_deletion():
+		_end_time_stop.rpc()
---
+++ game/skills/time_stop/use/time_stop_use.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+@onready var _world: World = get_tree().get_first_node_in_group(&"world")
+
+
+func _exit_tree() -> void:
+	if _world.process_mode != PROCESS_MODE_INHERIT:
+		resume_time()
+
+
+func stop_time() -> void:
+	_world.get_node(^"UI").process_mode = Node.PROCESS_MODE_PAUSABLE
+	_world.get_node(^"Camera").process_mode = Node.PROCESS_MODE_PAUSABLE
+	_world.process_mode = Node.PROCESS_MODE_DISABLED
+
+
+func resume_time() -> void:
+	_world.process_mode = Node.PROCESS_MODE_INHERIT
+	_world.get_node(^"UI").process_mode = Node.PROCESS_MODE_INHERIT
+	_world.get_node(^"Camera").process_mode = Node.PROCESS_MODE_INHERIT
+
+
+func safe_free() -> void:
+	if multiplayer.is_server():
+		queue_free()
