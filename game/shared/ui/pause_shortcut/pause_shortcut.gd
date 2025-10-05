544c2409c4cac89603981cc65a223e7fed9e8842
Summersay415 <summersay415@gmail.com>
2025-10-05 11:23:29 +0700

    Бафф наград, пара изменений баланса

--- game/shared/ui/pause_shortcut/pause_shortcut.gd
+++ game/shared/ui/pause_shortcut/pause_shortcut.gd
@@ -1,13 +1,17 @@
 extends Shortcut
 
-@export var controller_event: InputEvent
-@export var keyboard_and_mouse_event: InputEvent
 
-func _setup_local_to_scene() -> void:
+func _init() -> void:
 	match Globals.get_current_input_method():
 		Globals.InputMethod.KEYBOARD_AND_MOUSE:
-			events = [keyboard_and_mouse_event]
+			var event := InputEventAction.new()
+			event.action = &"pause"
+			event.pressed = true
+			events = [event]
 		Globals.InputMethod.CONTROLLER:
-			events = [controller_event]
+			var event := InputEventAction.new()
+			event.action = &"c_pause"
+			event.pressed = true
+			events = [event]
 		Globals.InputMethod.TOUCH:
 			events = []
