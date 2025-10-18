fccd4a6549ca6310abd3963d4b3a1e1114fd913f
Summersay415 <summersay415@gmail.com>
2025-10-18 13:07:32 +0700

    Новые скины: Злюка, Пофигист и Дьявол

---
+++ game/skins/emoji/pokerface/pokerface.gd
@@ -0,0 +1,15 @@
+extends PlayerSkin
+
+@onready var _anim: AnimationPlayer = $AnimationPlayer
+
+func _initialize() -> void:
+	player.health_changed.connect(_on_player_health_changed)
+
+
+func _on_player_health_changed(old: int, new: int) -> void:
+	if old > new:
+		_anim.play(&"hurt")
+		_anim.seek(0.0)
+	elif new > old:
+		_anim.play(&"heal")
+		_anim.seek(0.0)
