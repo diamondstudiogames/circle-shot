44970086ad2dd36c22ca139c53fdd05e9029c6a5
Summersay415 <summersay415@gmail.com>
2025-10-28 15:34:22 +0700

    Новое оружие - Миниган

--- game/weapons/guns/other/bazooka/bazooka.gd
+++ game/weapons/guns/other/bazooka/bazooka.gd
@@ -10,3 +10,7 @@ func _unmake_current() -> void:
 	super()
 	if ammo + ammo_in_stock <= 0 and player.current_weapon_type == Weapon.Type.ADDITIONAL:
 		player.set_weapon.call_deferred(Weapon.Type.ADDITIONAL, null)
+
+
+func get_ammo_text() -> String:
+	return "Осталось: %d" % (ammo + ammo_in_stock)
---
+++ game/weapons/guns/rifles/minigun/minigun.gd
@@ -0,0 +1,29 @@
+extends Gun
+
+
+@export var charge_time := 0.8
+@export var handle_max_rotation := -30.0
+@export var handle_rotation_speed := 37.5
+
+var _charge_timer := 0.0
+@onready var _handle: Sprite2D = $Visual/Base/Handle
+
+
+func _physics_process(delta: float) -> void:
+	_shoot_timer -= delta
+	if player.player_input.shooting and can_shoot():
+		_charge_timer += delta
+		_handle.rotation_degrees = move_toward(_handle.rotation_degrees, handle_max_rotation,
+				handle_rotation_speed * delta)
+	else:
+		_charge_timer = clampf(_charge_timer - delta, 0.0, charge_time)
+		_handle.rotation_degrees = move_toward(_handle.rotation_degrees, 0,
+				handle_rotation_speed * delta)
+	
+	if multiplayer.is_server() and can_shoot() and player.player_input.shooting \
+			and ammo >= ammo_per_shot and _charge_timer >= charge_time and _shoot_timer <= 0.0:
+		_update_rotation()
+		_update_rotation_position()
+		shoot()
+	if player.is_local() and can_reload() and ammo < ammo_per_shot:
+		player.try_reload_weapon()
