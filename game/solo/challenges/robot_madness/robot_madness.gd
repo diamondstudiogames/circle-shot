5659fb165fb47437a80d1dbd0310b5efc7954735
Summersay415 <summersay415@gmail.com>
2026-07-25 14:05:29 +0700

    Переработка Роборубки с системой волн

--- game/solo/challenges/robot_madness/robot_madness.gd
+++ game/solo/challenges/robot_madness/robot_madness.gd
@@ -3,54 +3,133 @@ extends Challenge
 
 ## Испытание "Рубка роботов".
 
+## Издаётся, когда волна начинается.
+signal wave_started
+## Издаётся, когда волна заканчивается.
+signal wave_ended
+## Издаётся, когда игрок прокачивается.
+signal player_upgraded
+
 @export_group("Tweaks")
-## Кривая множителя урона, где домен - время, значение - множитель.
+## Общее количество волн.
+@export var total_waves_count: int = 20
+## Кривая множителя урона, где домен - волна, значение - множитель.
 @export var damage_multiplier_curve: Curve
-## Кривая множителя здоровья, где домен - время, значение - множитель.
+## Кривая множителя здоровья, где домен - волна, значение - множитель.
 @export var health_multiplier_curve: Curve
-## Кривая множителя скорости, где домен - время, значение - множитель.
+## Кривая множителя скорости, где домен - волна, значение - множитель.
 @export var speed_multiplier_curve: Curve
-## Кривая интервала между появлениями роботов, где домен - время, значение - интервал.
+## Кривая интервала появления роботов, где домен - волна, значение - интервал.
 @export var spawn_interval_curve: Curve
-## Раз в сколько очков появляется аптечка.
-@export var points_for_heal_box: int = 50
-## Раз в сколько очков появляется коробка с боеприпасами.
-@export var points_for_ammo_box: int = 120
+## Кривая количества роботов в волне, где домен - волна, значение - количество.
+@export var count_curve: Curve
 ## Минимальное расстояние до игрока, на котором должен находиться появившийся робот. Если не
 ## соблюдено, выбирается другая точка.
 @export var safe_player_distance := 960.0
+## Максимальное случайное отклонение от количества создаваемых роботов на волне.
+@export var spawn_count_randomization: int = 1
+## Максимальное случайное отклонение от интервала между появлением роботов.
+@export var spawn_interval_randomization := 1.0
 
-@export_group("Rewards")
-## Сколько нужно выжить в секундах, чтобы получить монету.
-@export var seconds_survived_for_coin := 2.0
-## Сколько нужно получить очков, чтобы получить монету.
-@export var points_for_coin := 5.0
-## Сколько нужно нанести урона, чтобы получить монету.
-@export var damage_for_coin := 50.0
+@export_group("Points")
 ## Сколько очков получит игрок за убийство, умножённое на [member Entity.speed_mulitplier] жертвы.
 @export var points_for_speed_multiplier_base := 8.0
 ## Сколько очков получит игрок за убийство, умножённое на [member Entity.damage_mulitplier] жертвы.
 @export var points_for_damage_multiplier_base := 15.0
 ## Сколько очков получит игрок за убийство, умножённое на [member Entity.max_health] жертвы.
 @export var points_for_health_base := 0.1
+## Раз в сколько очков появляется аптечка.
+@export var points_for_heal_box: int = 50
+## Раз в сколько очков появляется коробка с боеприпасами.
+@export var points_for_ammo_box: int = 120
 
-## Сколько секунд игрок прожил.
-var time_survived: int = 0
-## Сколько очков набрал игрок.
+@export_group("Upgrades")
+@export_subgroup("Health", "health_")
+## На сколько процентов повышается здоровье за каждую прокачку.
+@export var health_increase_per_upgrade := 0.2
+## Базовая цена прокачки здоровья.
+@export var health_cost_base: int = 20
+## На сколько цена будет становиться больше за каждую прокачку здоровья.
+@export var health_cost_per_upgrade: int = 5
+## Максимальное количество прокачек здоровья.
+@export var health_max_upgrades: int = 15
+
+@export_subgroup("Damage", "damage_")
+## На сколько процентов повышается урон за каждую прокачку.
+@export var damage_increase_per_upgrade := 0.15
+## Базовая цена прокачки урона.
+@export var damage_cost_base: int = 25
+## На сколько цена будет становиться больше за каждую прокачку урона.
+@export var damage_cost_per_upgrade: int = 8
+## Максимальное количество прокачек урона.
+@export var damage_max_upgrades: int = 20
+
+@export_subgroup("Defense", "defense_")
+## На сколько будет повышаться защита за каждую прокачку.
+@export var defense_increase_per_upgrade: int = 1
+## Базовая цена прокачки защиты.
+@export var defense_cost_base: int = 20
+## На сколько цена будет становиться больше за каждую прокачку защиты.
+@export var defense_cost_per_upgrade: int = 8
+## Максимальное количество прокачек защиты.
+@export var defense_max_upgrades: int = 10
+
+@export_subgroup("Speed", "speed_")
+## На сколько процентов будет повышаться скорость за каждую прокачку.
+@export var speed_increase_per_upgrade := 0.1
+## Базовая цена прокачки скорости.
+@export var speed_cost_base: int = 30
+## На сколько цена будет становиться больше за каждую прокачку скорости.
+@export var speed_cost_per_upgrade: int = 10
+## Максимальное количество прокачек скорости.
+@export var speed_max_upgrades: int = 10
+
+@export_group("Restore")
+## Стоимость восстановления всех ОЗ.
+@export var health_restore_cost_per_wave: int = 30
+## Стоимость восстановления всех боеприпасов.
+@export var ammo_restore_cost_per_wave: int = 25
+## Сцена коробки с исцелением.
+@export var _heal_box_scene: PackedScene
+## Сцена коробки с боеприпасами.
+@export var _ammo_box_scene: PackedScene
+
+@export_group("Rewards")
+## Сколько монет получит игрок за полное прохождение испытания.
+@export var total_coins: int = 350
+## Кривая сглаживания, по которой будет вычислена награда за пройденные волны.
+@export_exp_easing var total_coins_ease := 2.0
+## Сколько нужно получить очков, чтобы получить монету.
+@export var points_for_coin := 5.0
+## Сколько нужно нанести урона, чтобы получить монету.
+@export var damage_for_coin := 50.0
+
+## Текущая волна.
+var current_wave: int = 0
+## Сколько всего очков набрал игрок.
+var total_points: int = 0
+## Сколько очков сейчас на балансе.
 var points: int = 0
+
+## Сколько раз было прокачено здоровье.
+var health_upgrades: int = 0
+## Сколько раз был прокачен урон.
+var damage_upgrades: int = 0
+## Сколько раз была прокачена скорость.
+var speed_upgrades: int = 0
+## Сколько раз была прокачена защита.
+var defense_upgrades: int = 0
 
 var _spawn_points: Array[Node2D]
 var _spawn_points_counter: int = 0
 var _robot_counter: int = 0
+var _alive_robots_counter: int = 0
 
 var _heal_box_points_counter: int = 0
 var _ammo_box_points_counter: int = 0
-var _heal_box_scene: PackedScene = load("uid://bysyaaj2r7stt")
-var _ammo_box_scene: PackedScene = load("uid://bdtqr6mv231py")
 
 ## Интерфейс этого испытания.
-@onready var robot_madness_ui: RobotMadnessUI = $UI
-@onready var _survived_timer: Timer = $SurvivedTimer
+@onready var _robot_madness_ui: RobotMadnessUI = $UI
 @onready var _spawn_timer: Timer = $SpawnTimer
 
 
@@ -60,22 +139,188 @@ func _initialize() -> void:
 
 
 func _finish_start() -> void:
-	_survived_timer.start()
-	_spawn_timer.start(spawn_interval_curve.sample(0.0))
+	start_wave()
 
 
 func _local_player_died() -> void:
 	end_challenge(false)
 	_spawn_timer.stop()
-	_survived_timer.stop()
 
 
 func _get_rewards() -> Dictionary[String, int]:
 	var rewards: Dictionary[String, int]
-	rewards["Прожито времени"] = roundi(time_survived / seconds_survived_for_coin)
-	rewards["Очки"] = roundi(points / points_for_coin)
+	if is_instance_valid(local_player):
+		rewards["Зачищенные волны"] = total_coins
+	else:
+		rewards["Зачищенные волны"] = roundi(total_coins
+				* ease((current_wave - 1.0) / total_waves_count, total_coins_ease))
+	rewards["Заработанные очки"] = roundi(total_points / points_for_coin)
 	rewards["Нанесённый урон"] = roundi(damaged / damage_for_coin)
 	return rewards
+
+
+## Начинает следующую волну.
+func start_wave() -> void:
+	if current_wave != 0:
+		unfreeze_entities()
+		other_parent.process_mode = Node.PROCESS_MODE_INHERIT
+	current_wave += 1
+	wave_started.emit()
+	_robot_madness_ui.start_wave()
+	
+	var robot_count: int = roundi(count_curve.sample(current_wave)) \
+			+ randi_range(-spawn_count_randomization, spawn_count_randomization)
+	var spawn_interval: float = spawn_interval_curve.sample(current_wave)
+	_alive_robots_counter = robot_count
+	for i: int in robot_count:
+		if not is_instance_valid(local_player):
+			break
+		_spawn_robot()
+		if i != robot_count - 1:
+			_spawn_timer.start(spawn_interval
+					+ randf_range(-spawn_interval_randomization, spawn_interval_randomization))
+			await _spawn_timer.timeout
+
+
+## Возвращает стоимоить прокачки здоровья.
+## Возвращает [code]-1[/code], если достигнут максимум улучшений.
+func get_health_upgrade_cost() -> int:
+	if health_upgrades >= health_max_upgrades:
+		return -1
+	return health_cost_base + health_cost_per_upgrade * health_upgrades
+
+
+## Возвращает стоимоить прокачки урона.
+## Возвращает [code]-1[/code], если достигнут максимум улучшений.
+func get_damage_upgrade_cost() -> int:
+	if damage_upgrades >= damage_max_upgrades:
+		return -1
+	return damage_cost_base + damage_cost_per_upgrade * damage_upgrades
+
+
+## Возвращает стоимоить прокачки здоровья.
+## Возвращает [code]-1[/code], если достигнут максимум улучшений.
+func get_defense_upgrade_cost() -> int:
+	if defense_upgrades >= defense_max_upgrades:
+		return -1
+	return defense_cost_base + defense_cost_per_upgrade * defense_upgrades
+
+
+## Возвращает стоимоить прокачки скорости.
+## Возвращает [code]-1[/code], если достигнут максимум улучшений.
+func get_speed_upgrade_cost() -> int:
+	if speed_upgrades >= speed_max_upgrades:
+		return -1
+	return speed_cost_base + speed_cost_per_upgrade * speed_upgrades
+
+
+## Прокачивает здоровье у игрока.
+func upgrade_health() -> void:
+	if not is_instance_valid(local_player):
+		return
+	local_player.max_health = get_player_max_health(true)
+	health_upgrades += 1
+	local_player.max_health = roundi(local_player.max_health
+			* (1.0 + health_upgrades * health_increase_per_upgrade))
+	local_player.heal(roundi(get_player_max_health(true) * health_increase_per_upgrade))
+	player_upgraded.emit()
+
+
+## Прокачивает урон у игрока.
+func upgrade_damage() -> void:
+	if not is_instance_valid(local_player):
+		return
+	local_player.damage_multiplier /= 1.0 + damage_upgrades * damage_increase_per_upgrade
+	damage_upgrades += 1
+	local_player.damage_multiplier *= 1.0 + damage_upgrades * damage_increase_per_upgrade
+	player_upgraded.emit()
+
+
+## Прокачивает скорость у игрока.
+func upgrade_speed() -> void:
+	if not is_instance_valid(local_player):
+		return
+	local_player.speed /= 1.0 + speed_upgrades * speed_increase_per_upgrade
+	speed_upgrades += 1
+	local_player.speed *= 1.0 + speed_upgrades * speed_increase_per_upgrade
+	player_upgraded.emit()
+
+
+## Прокачивает защиту у игрока.
+func upgrade_defense() -> void:
+	if not is_instance_valid(local_player):
+		return
+	defense_upgrades += 1
+	local_player.get_node(^"Defense").set(
+			&"defense", defense_upgrades * defense_increase_per_upgrade)
+	player_upgraded.emit()
+
+
+## Возвращает стоимость восстановления ОЗ у игрока. Возвращает [code]-1[/code], если ОЗ
+## на максимуме.
+func get_health_restore_cost() -> int:
+	if not is_instance_valid(local_player) \
+			or local_player.current_health == local_player.max_health:
+		return -1
+	return health_restore_cost_per_wave * current_wave
+
+
+## Возвращает стоимость восстановления боеприпасов у игрока. Возвращает [code]-1[/code], если
+## боеприпасы на максимуме.
+func get_ammo_restore_cost() -> int:
+	if not is_instance_valid(local_player):
+		return -1
+	for type: Weapon.Type in [
+		Weapon.Type.LIGHT,
+		Weapon.Type.HEAVY,
+		Weapon.Type.SUPPORT,
+		Weapon.Type.MELEE,
+	]:
+		var weapon := local_player.weapons.get_child(type) as Weapon
+		if not weapon or weapon.ammo_total <= 0:
+			continue
+		if weapon.ammo_in_stock != weapon.ammo_total - weapon.ammo_per_load:
+			return ammo_restore_cost_per_wave * current_wave
+	return -1
+
+
+## Восстанавливает все ОЗ у игрока.
+func restore_health() -> void:
+	if not is_instance_valid(local_player):
+		return
+	local_player.heal(local_player.max_health)
+
+
+## Восстанавливает все боеприпасы у игрока.
+func restore_ammo() -> void:
+	if not is_instance_valid(local_player):
+		return
+	
+	local_player.add_ammo_to_weapon.rpc(Weapon.Type.LIGHT, 1.0)
+	local_player.add_ammo_to_weapon.rpc(Weapon.Type.HEAVY, 1.0)
+	local_player.add_ammo_to_weapon.rpc(Weapon.Type.SUPPORT, 1.0)
+	local_player.add_ammo_to_weapon.rpc(Weapon.Type.MELEE, 1.0)
+
+
+## Возвращает максимальное здоровье игрока. Если [param default] равен [code]true[/code],
+## то возвращает максимальное здоровье без прокачки.
+func get_player_max_health(default: bool) -> int:
+	if not is_instance_valid(local_player):
+		return 0
+	if default:
+		return roundi(local_player.max_health
+				/ (1.0 + health_upgrades * health_increase_per_upgrade))
+	return local_player.max_health
+
+
+func _end_wave() -> void:
+	wave_ended.emit()
+	if current_wave == total_waves_count:
+		end_challenge(true)
+		return
+	_robot_madness_ui.end_wave()
+	freeze_entities()
+	other_parent.process_mode = Node.PROCESS_MODE_DISABLED # чтобы коробки не деспавнились
 
 
 func _spawn_robot() -> void:
@@ -92,12 +337,12 @@ func _spawn_robot() -> void:
 			spawn_pos = _spawn_points[_spawn_points_counter].global_position
 	
 	robot.position = spawn_pos
-	robot.team = 1
+	robot.team = Entity.Team.BLUE
 	robot.id = -randi()
 	robot.name += str(robot.id)
-	robot.max_health = roundi(robot.max_health * health_multiplier_curve.sample(time_survived))
-	robot.damage_multiplier = damage_multiplier_curve.sample(time_survived)
-	robot.speed_multiplier = speed_multiplier_curve.sample(time_survived)
+	robot.max_health = roundi(robot.max_health * health_multiplier_curve.sample(current_wave))
+	robot.damage_multiplier = damage_multiplier_curve.sample(current_wave)
+	robot.speed_multiplier = speed_multiplier_curve.sample(current_wave)
 	robot.died.connect(_on_robot_died.bind(robot))
 	entities_parent.add_child(robot, true)
 	
@@ -129,6 +374,7 @@ func _on_robot_died(robot: Entity) -> void:
 			+ roundi(robot.speed_multiplier * points_for_speed_multiplier_base) \
 			+ roundi(robot.max_health * points_for_health_base)
 	points += got_points
+	total_points += got_points
 	stats_changed.emit()
 	
 	_heal_box_points_counter += got_points
@@ -137,8 +383,8 @@ func _on_robot_died(robot: Entity) -> void:
 			and _ammo_box_points_counter < points_for_ammo_box:
 		_heal_box_points_counter = 0
 		_spawn_heal_box(robot.global_position)
-	elif _ammo_box_points_counter < points_for_ammo_box \
-			and _heal_box_points_counter >= points_for_heal_box:
+	elif _ammo_box_points_counter >= points_for_ammo_box \
+			and _heal_box_points_counter < points_for_heal_box:
 		_ammo_box_points_counter = 0
 		_spawn_ammo_box(robot.global_position)
 	elif _heal_box_points_counter >= points_for_heal_box \
@@ -154,17 +400,11 @@ func _on_robot_died(robot: Entity) -> void:
 				_spawn_ammo_box(robot.global_position)
 		_ammo_box_points_counter = 0
 		_heal_box_points_counter = 0
+	
+	_alive_robots_counter -= 1
+	if _alive_robots_counter == 0:
+		_end_wave()
 
 
 func _on_stats_changed() -> void:
-	robot_madness_ui.set_stats(damaged, points, time_survived)
-
-
-func _on_survived_timer_timeout() -> void:
-	time_survived += 1
-	stats_changed.emit()
-
-
-func _on_spawn_timer_timeout() -> void:
-	_spawn_robot()
-	_spawn_timer.start(spawn_interval_curve.sample(time_survived))
+	_robot_madness_ui.update_stats()
--- game/solo/challenges/robot_madness/robot_madness_ui.gd
+++ game/solo/challenges/robot_madness/robot_madness_ui.gd
@@ -2,12 +2,133 @@ class_name RobotMadnessUI
 extends ChallengeUI
 ## Интерфейс испытания "Рубка роботов".
 
+@onready var _robot_madness: RobotMadness = get_parent()
 @onready var _stats_label: Label = $Main/Stats
 
-## Устанавливает текущие показатели.
-func set_stats(damaged: int, points: int, time: int) -> void:
-	var minutes: int = floori(time / 60.0)
-	var seconds: int = time % 60
-	_stats_label.text = "Нанесено урона: %d" % damaged
-	_stats_label.text += "\nОчки: %d" % points
-	_stats_label.text += "\nВремени прожито: %02d:%02d" % [minutes, seconds]
+## Обновляет текущие показатели.
+func update_stats() -> void:
+	_stats_label.text = "Нанесено урона: %d" % _robot_madness.damaged
+	_stats_label.text += "\nОчки: %d" % _robot_madness.points
+	_stats_label.text += "\nВолна: %d" % _robot_madness.current_wave
+
+
+## Начинает волну: скрывает магазин и показывает сообщение о начале волны.
+## Также вызывает [method update_stats].
+func start_wave() -> void:
+	update_stats()
+	($Main/WaveInfo/AnimationPlayer as AnimationPlayer).play(&"wave_start")
+	($Main/WaveInfo as Label).text = "Волна %d началась!" % _robot_madness.current_wave
+	($Main/Shop as CanvasItem).hide()
+
+
+## Заканчивает волну: показывает сообщение, а затем и магазин.
+func end_wave() -> void:
+	($Main/WaveInfo/AnimationPlayer as AnimationPlayer).play(&"wave_end")
+	($Main/WaveInfo as Label).text = "Волна зачищена!"
+	await ($Main/WaveInfo/AnimationPlayer as AnimationMixer).animation_finished
+	($Main/Shop as CanvasItem).show()
+	_update_upgrades()
+
+
+func _update_upgrades() -> void:
+	var restore_health: Button = %Restore/Health
+	var restore_health_cost: int = _robot_madness.get_health_restore_cost()
+	restore_health.disabled = restore_health_cost < 0 or _robot_madness.points < restore_health_cost
+	restore_health.text = "Восстановить ОЗ (%s)" % \
+			("%d очк." % restore_health_cost if restore_health_cost > 0 else "макс.")
+	
+	var restore_ammo: Button = %Restore/Ammo
+	var restore_ammo_cost: int = _robot_madness.get_ammo_restore_cost()
+	restore_ammo.disabled = restore_ammo_cost < 0 or _robot_madness.points < restore_ammo_cost
+	restore_ammo.text = "Восстановить боеприпасы (%s)" % \
+			("%d очк." % restore_ammo_cost if restore_ammo_cost > 0 else "макс.")
+	
+	var health_label: Label = %Health/Container/Label
+	var health_upgrade: Button = %Health/Container/Upgrade
+	var health_upgrade_cost: int = _robot_madness.get_health_upgrade_cost()
+	health_label.text = "Здоровье: %d" % _robot_madness.get_player_max_health(false)
+	health_upgrade.disabled = health_upgrade_cost < 0 or _robot_madness.points < health_upgrade_cost
+	health_upgrade.text = "+%d (%s)" % [
+		_robot_madness.get_player_max_health(true) * _robot_madness.health_increase_per_upgrade,
+		"%d очк." % health_upgrade_cost if health_upgrade_cost > 0 else "макс.",
+	]
+	
+	var defense_label: Label = %Defense/Container/Label
+	var defense_upgrade: Button = %Defense/Container/Upgrade
+	var defense_upgrade_cost: int = _robot_madness.get_defense_upgrade_cost()
+	defense_label.text = "Защита: %d" % \
+			(_robot_madness.defense_upgrades * _robot_madness.defense_increase_per_upgrade)
+	defense_upgrade.disabled = defense_upgrade_cost < 0 \
+			or _robot_madness.points < defense_upgrade_cost
+	defense_upgrade.text = "+%d (%s)" % [
+		_robot_madness.defense_increase_per_upgrade,
+		"%d очк." % defense_upgrade_cost if defense_upgrade_cost > 0 else "макс.",
+	]
+	
+	var damage_label: Label = %Damage/Container/Label
+	var damage_upgrade: Button = %Damage/Container/Upgrade
+	var damage_upgrade_cost: int = _robot_madness.get_damage_upgrade_cost()
+	damage_label.text = "Урон: +%d%%" % roundi(100 * _robot_madness.damage_upgrades
+			* _robot_madness.damage_increase_per_upgrade)
+	damage_upgrade.disabled = damage_upgrade_cost < 0 or _robot_madness.points < damage_upgrade_cost
+	damage_upgrade.text = "+%d%% (%s)" % [
+		roundi(_robot_madness.damage_increase_per_upgrade * 100),
+		"%d очк." % damage_upgrade_cost if damage_upgrade_cost > 0 else "макс.",
+	]
+	
+	var speed_label: Label = %Speed/Container/Label
+	var speed_upgrade: Button = %Speed/Container/Upgrade
+	var speed_upgrade_cost: int = _robot_madness.get_speed_upgrade_cost()
+	speed_label.text = "Скорость: +%d%%" % roundi(100 * _robot_madness.speed_upgrades
+			* _robot_madness.speed_increase_per_upgrade)
+	speed_upgrade.disabled = speed_upgrade_cost < 0 or _robot_madness.points < speed_upgrade_cost
+	speed_upgrade.text = "+%d%% (%s)" % [
+		roundi(_robot_madness.speed_increase_per_upgrade * 100),
+		"%d очк." % speed_upgrade_cost if speed_upgrade_cost > 0 else "макс.",
+	]
+
+
+func _on_player_upgraded() -> void:
+	_update_upgrades()
+
+
+func _on_next_wave_pressed() -> void:
+	_robot_madness.start_wave()
+
+
+func _on_restore_health_pressed() -> void:
+	_robot_madness.points -= _robot_madness.get_health_restore_cost()
+	_robot_madness.restore_health()
+	update_stats()
+	_update_upgrades()
+
+
+func _on_restore_ammo_pressed() -> void:
+	_robot_madness.points -= _robot_madness.get_ammo_restore_cost()
+	_robot_madness.restore_ammo()
+	update_stats()
+	_update_upgrades()
+
+
+func _on_upgrade_health_pressed() -> void:
+	_robot_madness.points -= _robot_madness.get_health_upgrade_cost()
+	_robot_madness.upgrade_health()
+	update_stats()
+
+
+func _on_upgrade_damage_pressed() -> void:
+	_robot_madness.points -= _robot_madness.get_damage_upgrade_cost()
+	_robot_madness.upgrade_damage()
+	update_stats()
+
+
+func _on_upgrade_defense_pressed() -> void:
+	_robot_madness.points -= _robot_madness.get_defense_upgrade_cost()
+	_robot_madness.upgrade_defense()
+	update_stats()
+
+
+func _on_upgrade_speed_pressed() -> void:
+	_robot_madness.points -= _robot_madness.get_speed_upgrade_cost()
+	_robot_madness.upgrade_speed()
+	update_stats()
